0419b255ca19d2038246d3569f3cd7908de54e7e
Colden Prime <coldenprime@gmail.com>
2014-06-07 13:25:06 -0400

    Add Client Xcode Project

---
+++ PayMobileClient/PayMobileClientTests/PayMobileClientTests.m
@@ -0,0 +1,34 @@
+//
+//  PayMobileClientTests.m
+//  PayMobileClientTests
+//
+//  Created by Colden Prime on 6/7/14.
+//  Copyright (c) 2014 IntrepidPursuits. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PayMobileClientTests : XCTestCase
+
+@end
+
+@implementation PayMobileClientTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
