f9a109f12bff58c169bb8b50711c5a2c6dcb44f4
Guy <guy@intrepid.io>
2014-06-07 19:09:11 -0400

    added image files and fleshed out PayImages class -> 3 methods returning UIImages

--- PayMobileClient/PayMobileClient/PayImages.m
+++ PayMobileClient/PayMobileClient/PayImages.m
@@ -13,21 +13,89 @@
 + (UIImage *)iconImageWithPaymentType:(PaymentType)paymentType {
     NSString *filename = nil;
     switch (paymentType) {
+        case PaymentTypeBitPay:
         case PaymentTypeBitCoin:
+            filename = @"bitcoin_color_144.png";
             break;
         case PaymentTypeStarbucks:
+            filename = @"starbucks_color_144.png";
             break;
         case PaymentTypePayPal:
+            filename = @"paypal_color_144.png";
             break;
         case PaymentTypeLevelUp:
+            filename = @"levelup_color_144.png";
             break;
         case PaymentTypeDunkinDonuts:
+            filename = @"dunkin_color_144.png";
             break;
         case PaymentTypeCumberlandFarms:
-            break;
-        case PaymentTypeBitPay:
+            filename = @"cfarms_color_144.png";
             break;
         case PaymentTypeLeaf:
+            filename = @"leaf_color_144.png";
+            break;
+        default:
+            break;
+    }
+    return [UIImage imageNamed:filename];
+}
+
++ (UIImage *)greyIconImageWithPaymentType:(PaymentType)paymentType {
+    NSString *filename = nil;
+    switch (paymentType) {
+        case PaymentTypeBitPay:
+        case PaymentTypeBitCoin:
+            filename = @"bitcoin_grey_144.png";
+            break;
+        case PaymentTypeStarbucks:
+            filename = @"starbucks_grey_144.png";
+            break;
+        case PaymentTypePayPal:
+            filename = @"paypal_grey_144.png";
+            break;
+        case PaymentTypeLevelUp:
+            filename = @"levelup_grey_144.png";
+            break;
+        case PaymentTypeDunkinDonuts:
+            filename = @"dunkin_grey_144.png";
+            break;
+        case PaymentTypeCumberlandFarms:
+            filename = @"cfarms_grey_144.png";
+            break;
+        case PaymentTypeLeaf:
+            filename = @"leaf_grey_144.png";
+            break;
+        default:
+            break;
+    }
+    return [UIImage imageNamed:filename];
+}
+
++ (UIImage *)pinImageWithPaymentType:(PaymentType)paymentType {
+    NSString *filename = nil;
+    switch (paymentType) {
+        case PaymentTypeBitPay:
+        case PaymentTypeBitCoin:
+            filename = @"bitcoin_pin_144.png";
+            break;
+        case PaymentTypeStarbucks:
+            filename = @"starbucks_pin_144.png";
+            break;
+        case PaymentTypePayPal:
+            filename = @"paypal_pin_144.png";
+            break;
+        case PaymentTypeLevelUp:
+            filename = @"levelup_pin_144.png";
+            break;
+        case PaymentTypeDunkinDonuts:
+            filename = @"dunkin_pin_144.png";
+            break;
+        case PaymentTypeCumberlandFarms:
+            filename = @"cfarms_pin_144.png";
+            break;
+        case PaymentTypeLeaf:
+            filename = @"leaf_pin_144.png";
             break;
         default:
             break;
